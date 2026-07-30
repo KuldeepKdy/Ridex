b39b169fc1fd9fb85e5286c9b69bf0bcb8723f10
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2026-07-30 14:39:59 +0530

    OAuth credential and google backend setup done

---
+++ src/auth.ts
@@ -0,0 +1,97 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import connectDB from "./lib/db";
+import User from "./models/user.model";
+import Google from "next-auth/providers/google";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {
+          type: "email",
+          label: "Email",
+          placeholder: "johndoe@gmail.com",
+        },
+        password: {
+          type: "password",
+          label: "Password",
+          placeholder: "*****",
+        },
+      },
+      async authorize(credentials,request) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing credentials");
+        }
+
+        const email = credentials.email;
+        const password = credentials.password as string;
+        await connectDB();
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("User does not exist");
+        }
+        const isMatch = await user.comparePassword(password, user.password);
+        if (!isMatch) {
+          throw new Error("Incorrect password");
+        }
+        return {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        };
+      }
+    }),
+    Google({
+        clientId: process.env.AUTH_GOOGLE_ID,
+        clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    })
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider == "google") {
+        await connectDB();
+        const dbUser = await User.findOne({ email: user.email });
+        if (!dbUser) {
+          await User.create({
+            name: user.name,
+            email: user.email,
+          });
+        }
+        user.id = dbUser._id;
+        user.role = dbUser.role;
+      }
+      return true;
+    },
+    async jwt({token , user}) {
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.role = user.role;
+
+        return token;
+    },
+    async session({session, token}) {
+        if (session.user) {
+            session.user.id = token.id as string;
+            session.user.name = token.name;
+            session.user.email = token.email as string;
+            session.user.role = token.role as string;
+        }
+
+        return session;
+    },
+  },
+  pages: {
+    signIn: "/signin",
+    signOut: "/signout",
+    error: "/error",
+  },
+  session:{
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  secret: process.env.AUTH_SECRET,
+
+});
