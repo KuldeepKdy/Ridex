07384538b2263e5a823497867ec65ca8c8d5a3f5
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2026-07-31 12:10:29 +0530

    navbar frontend changes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,48 @@
-"use client"
-import React from 'react'
-import {motion} from "motion/react";
+"use client";
+import React from "react";
+import { motion } from "motion/react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Nav_Items = ["Home", "About", "Contact", "Booking"];
 
 function Nav() {
+  const pathName = usePathname();
   return (
-    <motion.div initial={{ y: -60 , opacity: 0 }} animate={{ y:0 , opacity: 1 }} className="fixed top-3 left-0 right-1/2 -translate-x-1/2 z-50 flex items-center justify-between h-16 px-4 bg-white shadow-md">
+    <motion.div
+      initial={{ y: -60, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      className="fixed top-3 left-1/2 -translate-x-1/2 w-[94%] md:w-[86%] z-50 rounded-full bg-[#0B0B0B] text-white shadow-[0_15px_50px_rgba(0,0,0,,0.7)] py-3"
+    >
+      <div className="max-w-7xl mx-auto px-4 md:px-8 flex items-center justify-between">
+        <Image src="/logo.png" alt="logo" width={44} height={44} />
 
+        <div className="hidden md:flex items-center gap-10">
+          {Nav_Items.map((item, index) => {
+            let href;
+            if (item === "Home") {
+              href = `/`;
+            } else {
+              href = `/${item.toLowerCase()}`;
+            }
+            const active = href === pathName;
+            return (
+              <Link
+                key={index}
+                href={href}
+                className={` text-sm  font-medium transition ${active ? "text-white" : "text-gray-400 hover:text-white"} `}
+              >
+                {item}
+              </Link>
+            );
+          })}
+        </div>
+
+        <button className="px-4 py-1.5 rounded-full bg-white text-black text-sm">Login</button>
+      </div>
     </motion.div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
