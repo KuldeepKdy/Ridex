0bf3544162228c00262abe9f5a0eb877c58b61a8
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2026-08-07 16:16:21 +0530

    Redux toolkit setup for accessing user data at anywhere

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/lib/Provider";
+import ReduxProvider from "@/redux/ReduxProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Provider>{children}</Provider>
+        <Provider>
+          <ReduxProvider>
+          {children}
+          </ReduxProvider>
+          </Provider>
       </body>
     </html>
   );
---
+++ src/redux/userSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { IUser } from "@/models/user.model";
+
+
+// Define a type for the slice state
+interface IuserState {
+  userData: IUser | null; // Replace 'any[]' with the actual type for user data
+}
+
+// Define the initial state using that type
+const initialState: IuserState = {
+  userData: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    setUserData: (state, action) => {
+      state.userData = action.payload;
+    }
+  },
+});
+
+export const { setUserData } = userSlice.actions;
+
+
+
+export default userSlice.reducer;
