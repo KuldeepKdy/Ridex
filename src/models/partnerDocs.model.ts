61815da311fdc60b56d45c45eaeb5af04b99ba55
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2026-08-10 16:56:14 +0530

    partner onboard page vehicle,partnerdocs and partner bank schema models

---
+++ src/models/partnerDocs.model.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+interface IPartnerDocs {
+  owner: mongoose.Types.ObjectId;
+  aadharUrl: string;
+  rcUrl: string;
+  licenseUrl: string;
+  status: "pending" | "approved" | "rejected";
+  rejectionReason?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const partnerDocsSchema = new mongoose.Schema<IPartnerDocs>(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    aadharUrl: {
+      type: String,
+      required: true,
+    },
+    rcUrl: {
+      type: String,
+      required: true,
+    },
+    licenseUrl: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "approved", "rejected"],
+    },
+    rejectionReason: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+const PartnerDocs =
+  mongoose.models.PartnerDocs ||
+  mongoose.model("PartnerDocs", partnerDocsSchema);
+
+export default PartnerDocs;
---
+++ src/models/vehicle.model.ts
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+type VehicleType = "bike" | "car" | "loading" | "auto" | "truck";
+
+interface IVehicle {
+  owner: mongoose.Types.ObjectId;
+  type: VehicleType;
+  vehicleModel: string;
+  number: string;
+  imageUrl?: string;
+  baseFare?: number;
+  pricePerKM?: number;
+  waitingCharge?: number;
+  status: "pending" | "approved" | "rejected";
+  rejectionReason?: string;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const vehicleSchema = new mongoose.Schema<IVehicle>(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["bike", "car", "loading", "auto", "truck"],
+    },
+    vehicleModel: {
+      type: String,
+      required: true,
+    },
+    number: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    imageUrl: {
+      type: String,
+    },
+    baseFare: {
+      type: Number,
+    },
+    pricePerKM: {
+      type: Number,
+    },
+    waitingCharge: {
+      type: Number,
+    },
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "approved", "rejected"],
+    },
+    rejectionReason: {
+      type: String,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Vehicle =
+  mongoose.models.Vehicle || mongoose.model("Vehicle", vehicleSchema);
+
+export default Vehicle;
