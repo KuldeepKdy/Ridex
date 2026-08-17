2b2060019c757815249f1b2be27f47ca2caec666
Kuldeep Yadav <kuldeepkdy4@gmail.com>
2026-08-17 16:46:01 +0530

    fixed session cache user role issue in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 import PartnerDashboard from "@/components/PartnerDashboard";
 import PublicHome from "@/components/PublicHome";
+import connectDB from "@/lib/db";
+import User from "@/models/user.model";
 import Image from "next/image";
 
 export default async function Home() {
+  await connectDB();
   const session = await auth();
+  const user = await User.findOne({ email: session?.user?.email });
   return (
     <div className="w-full min-h-screen bg-white">
       <Nav />
-      {session?.user?.role == "partner" ? (
+      {user?.role == "partner" ? (
         <PartnerDashboard />
-      ) : session?.user?.role == "admin" ? (
+      ) : user?.role == "admin" ? (
         <AdminDashboard />
       ) : (
         <PublicHome />
